5bb48e3880abab7d3a3774929c36af34991c2c12
natalia <natka545@mail.ru>
2022-11-27 15:23:54 +0300

    написала программу

--- Program.cs
+++ Program.cs
@@ -2,3 +2,28 @@
 //  либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 //  либо задать на старте выполнения алгоритма.
 //  При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+Console.Clear();
+string[] array1 = new string[7] {"987", "56", "world", "exzample", "final", " Hello", "78945"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
